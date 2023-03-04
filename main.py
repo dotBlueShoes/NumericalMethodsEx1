81552b611112440e2ac04cb14deb09be6fcc72f9
dotBlueShoes <matthewstrumillo@gmail.com>
2023-03-04 17:51:24 +0100

    marge & polynomial function counting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import csv
+from typing import List
 import numpy
 import statistics as st
 
@@ -162,47 +162,6 @@ def wyk_pudelkowy(lista1, lista2, lista3):
     plt.ylabel('Szerokość (cm)')
     plt.show()
 
-def main():
-    with open('data.csv', mode='r') as file:
-        lines = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-        licz_stosa = 0
-        licz_versicolor = 0
-        licz_virginica = 0
-        tab_stosa = []
-        tab_versicolor = []
-        tab_virginica = []
-        for line in lines:
-            kwiat = Kwiat()
-            kwiat.sepal_length = line[0]
-            kwiat.sepal_width = line[1]
-            kwiat.petal_length = line[2]
-            kwiat.petal_width = line[3]
-            if line[4] == 0:
-                print("setosa")
-                tab_stosa.append(kwiat)
-                licz_stosa = licz_stosa + 1
-                tab_stosa[licz_stosa - 1].print_stat()
-            elif line[4] == 1:
-                print("versicolor")
-                tab_versicolor.append(kwiat)
-                licz_versicolor = licz_versicolor + 1
-                tab_versicolor[licz_versicolor - 1].print_stat()
-            elif line[4] == 2:
-                print("virginica")
-                tab_virginica.append(kwiat)
-                licz_virginica = licz_virginica + 1
-            else:
-                print("Wrong data")
-        liczebnosc = licz_stosa + licz_versicolor + licz_virginica
-        tab1("setosa", licz_stosa, liczebnosc)
-        tab1("versicolor", licz_versicolor, liczebnosc)
-        tab1("virginica", licz_virginica, liczebnosc)
-        tab1("Razem",liczebnosc,liczebnosc)
-        tab2(tab_stosa, tab_versicolor, tab_virginica, liczebnosc)
-        wykresy(tab_stosa, tab_versicolor, tab_virginica)
-        wyk_pudelkowy(tab_stosa, tab_versicolor, tab_virginica)
-
-
 def exponential_fuction():
     print("Funkcja wykładnicza ma postać f(x)=a*e^(bx)")
     a = input("Podaj wartość a:")
@@ -211,6 +170,108 @@ def exponential_fuction():
 def value_exponential_fuction(a,b,x):
     e = numpy.e
     return a*(e**(b*x))
+
+# Celem zadania pierwszego jest zaimplementowanie i porównanie ze sobą dwóch metod rozwiązywania 
+#  (znajdowania miejsca zerowego) równań nieliniowych. 
+# Implementacja Metody Bisekcji oraz Metoda Stycznych.
+#
+# 1. Osiągnięcie zadanej dokładności obliczeń (wariant A lub B powyżej); 
+# 2. Wykonanie określonej przez użytkownika liczby iteracji.
+#
+# Program ma mieć wbudowane kilka różnych funkcji nieliniowych: wielomian, trygonometryczną, wykładniczą i ich złożenia. 
+# Użytkownik wybiera jedną z funkcji, określa przedział na którym poszukiwane jest miejsce zerowe oraz wybiera 
+# kryterium zatrzymania algorytmu: 
+#   a) spełnienie warunku nałożonego na dokładność [|x(i) − x(i−1)| < ε]
+#   b) osiągnięcie zadanej liczby iteracji. 
+# Następnie użytkownik wprowadza ε (w przypadku wybrania pierwszego kryterium) lub 
+#  liczbę iteracji (w przypadku wyboru drugiego kryterium). 
+#
+# Program wykonuje obliczenia przy użyciu obu metod (bisekcja oraz jeden z przydzielonych wariantów), 
+#  wyświetla wyniki i rysuje wykres wybranej funkcji na zadanym przedziale, 
+#  zaznaczając rozwiązania na wykresie. 
+#
+# Program ma sprawdzać poprawność założenia o przeciwnych znakach funkcji na krańcach badanego przedziału. 
+# Nie trzeba sprawdzać prawdziwości założeń o stałym znaku pochodnych na przedziale. 
+# W przypadku metody stycznych dozwolone jest zakodowanie wartości pochodnej na sztywno, nie trzeba jej liczyć numerycznie.
+
+# Wielomian
+# W(x) = 1x^4 + 1x^3 + 1x^2 + 1x + 1 
+
+
+def calc_polynomial_function(polynomial: List[int], x_value: int) -> int:
+    polynomial_degree: int = len(polynomial)
+    result: int = 0
+    sum: int = 0
+
+    for i in range(0, polynomial_degree):
+        sum = polynomial[i]
+
+        for j in range(polynomial_degree - i - 1):
+            sum = sum * x_value
+
+        result += sum
+    return result
+
+polynomial_values: List[int]
+input_number: int = 0
+
+def main():
+
+    print("Wprowadz stopien wilomianu: ")
+    input_number = int(input())             # get polygonal rank - length of the array.
+    polynomial_values = [0] * input_number  # initialize array with 0's.
+
+    # Get a, b, c, d, ... values.
+    for i in range(0, input_number):
+        print("Wprowadz wartosc " + str(i + 1) + " skladnika wilomianu: ")
+        polynomial_values[i] = int(input())
+
+    # Get x value
+    print("Wprowadz wartosc x: ")
+    input_number = int(input())
+    result = calc_polynomial_function(polynomial_values, input_number)
+    print("Wynik wynosi: " + str(result))
+
+    input_number = input()
+
+    #with open('dane/data.csv', mode='r') as file:
+    #    lines = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    #    licz_stosa = 0
+    #    licz_versicolor = 0
+    #    licz_virginica = 0
+    #    tab_stosa = []
+    #    tab_versicolor = []
+    #    tab_virginica = []
+    #    for line in lines:
+    #        kwiat = Kwiat()
+    #        kwiat.sepal_length = line[0]
+    #        kwiat.sepal_width = line[1]
+    #        kwiat.petal_length = line[2]
+    #        kwiat.petal_width = line[3]
+    #        if line[4] == 0:
+    #            print("setosa")
+    #            tab_stosa.append(kwiat)
+    #            licz_stosa = licz_stosa + 1
+    #            tab_stosa[licz_stosa - 1].print_stat()
+    #        elif line[4] == 1:
+    #            print("versicolor")
+    #            tab_versicolor.append(kwiat)
+    #            licz_versicolor = licz_versicolor + 1
+    #            tab_versicolor[licz_versicolor - 1].print_stat()
+    #        elif line[4] == 2:
+    #            print("virginica")
+    #            tab_virginica.append(kwiat)
+    #            licz_virginica = licz_virginica + 1
+    #        else:
+    #            print("Wrong data")
+    #    liczebnosc = licz_stosa + licz_versicolor + licz_virginica
+    #    tab1("setosa", licz_stosa, liczebnosc)
+    #    tab1("versicolor", licz_versicolor, liczebnosc)
+    #    tab1("virginica", licz_virginica, liczebnosc)
+    #    tab1("Razem",liczebnosc,liczebnosc)
+    #    tab2(tab_stosa, tab_versicolor, tab_virginica, liczebnosc)
+    #    wykresy(tab_stosa, tab_versicolor, tab_virginica)
+    #    wyk_pudelkowy(tab_stosa, tab_versicolor, tab_virginica)
 
 
 if __name__ == '__main__':
